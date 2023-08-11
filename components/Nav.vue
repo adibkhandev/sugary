93d1362a5b4f65733c1dcfa447f0eb97ba2b9498
adibkhandev <adibkhaan001@gmail.com>
2023-08-12 03:09:18 +0600

    1.1

--- components/Nav.vue
+++ components/Nav.vue
@@ -8,13 +8,20 @@
 <template>
 	<div class="nav">
 		<div class="logo">
-			<h1>Sugary</h1>
+			<img src="icon-dot.svg" alt=""/>
 		</div>
 		<div 
 		    class="mobile-text-nav"
               :class="{mobileActivate:navActive}"
 		    >
-				
+		    <div class="links">
+				<a class="link" href="">How?</a>
+				<a class="link" href="">Browse Gifts</a>
+				<a class="link" href="">ForBuisness</a>
+				<a class="link" href="">Recetly served</a>
+				<a class="link" href="">About</a>
+		    </div>
+		    	
 		</div>
 		<div class="buttons">
 			<div class="text-nav">
@@ -26,9 +33,9 @@
 			</div>
 			<button @click="switchNavState()" >
 				<svg class="hamburger" viewBox="0 0 100 100" width="30" >
-					<rect class="top"  width="70" height="7" x="10" y="25" rx="5"/>
+					<rect class="top"  width="70" height="7" x="15" y="25" rx="5"/>
 					<rect class="middle"  width="70" height="7" x="10" y="45" rx="5"/>
-					<rect class="bottom"  width="70" height="7" x="10" y="65" rx="5"/>
+					<rect class="bottom"  width="70" height="7" x="5" y="65" rx="5"/>
 
 				</svg>
 			</button>
@@ -45,10 +52,10 @@
      	transition:top 1s ease-in-out;
      }
 	.nav{
-		background:rgba(0,0,0,0.50);
+		// background:rgba(0,0,0,0.50);
 		margin:0;
 		padding:0;
-		backdrop-filter:blur(10px);
+		// backdrop-filter:blur(10px);
 		position:sticky;
 		top:0em;
         z-index: 2;
@@ -57,6 +64,11 @@
 		display:flex;
 		justify-content:space-between;
 		align-items:center;
+		@media (min-width:950px){
+           backdrop-filter:blur(5px);
+           background:rgba(0,0,0,0.20);
+
+		}
 		.mobile-text-nav{
 			position:absolute;
 			top:-100vh;
@@ -65,17 +77,35 @@
 			width:100vw;
 			transition:top 1s ease-in-out;
 			backdrop-filter:blur(10px);
+			display: flex;
+			justify-content:center;
+			align-items:center;
+			z-index:-1;
+			.links{
+				width: 100%;
+				height: 80%;
+				display: flex;
+				flex-direction:column;
+				justify-content:flex-start;
+				align-items:center;
+			}
+			a{
+				margin-right:2rem;
+			   font-family:Poppins;
+			   font-weight:800;
+			   color: white;
+			   margin-top: 4rem;
+			}
 		}
 	}
 	.logo{
-        h1{
-		 color:white;
-		 opacity: 0.9;
-		 font-family:"Affectionate";
-		 font-size:clamp(1rem,2vw,1.4rem);
-		 line-height:2;
-		 margin-left:1.8em;
-		 height:100%;
+		height: 100%;
+		display: flex;
+		justify-content:center;
+        img{
+		  width: clamp(4.6rem,7vw,8rem);
+		  margin-left:clamp(1.2rem,1.6vw,3rem);
+		 
 		}
 	}
 	.buttons{
@@ -103,12 +133,13 @@
 		text-transform:uppercase;
 		font-size:clamp(0.7rem,1vw,0.8rem);
 		color:white; 
-		opacity:0.9;
+		
 		@media (max-width:950px) {
 				display:none;
 		}
 		.link{
-			margin-right:2rem;
+			font-size: inherit;
+			margin-right:5em;
 			font-family:Poppins;
 			font-weight:800;
 		}
